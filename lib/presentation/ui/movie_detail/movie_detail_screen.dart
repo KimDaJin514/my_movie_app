3ac665aa72362990ff58d0a0ad78ee50acfc1d41
김다진 <kimdajin514@gmail.com>
2024-10-23 14:02:25 +0900

    :recycle: 영화 기본 정보 리팩토링

--- lib/presentation/ui/movie_detail/movie_detail_screen.dart
+++ lib/presentation/ui/movie_detail/movie_detail_screen.dart
@@ -14,6 +14,8 @@ import 'package:my_movie_app/presentation/ui/movie_detail/bloc/movie_detail_bloc
 import 'package:my_movie_app/presentation/util/date_time_util.dart';
 import 'package:my_movie_app/presentation/util/int_extension.dart';
 
+part 'movie_basic_info_view.dart';
+
 @RoutePage()
 class MovieDetailScreen extends StatelessWidget {
   final int movieId;
@@ -72,177 +74,36 @@ class _MovieDetailViewState extends State<_MovieDetailView> {
   @override
   Widget build(BuildContext context) {
     _topViewPadding = MediaQuery.of(context).viewPadding.top;
-    return BlocSelector<MovieDetailBloc, MovieDetailState, MovieVo>(
-      selector: (state) => state.movieVo,
-      builder: (context, movie) {
-        return Scaffold(
-          backgroundColor: gray950,
-          body: CustomScrollView(
-            controller: _scrollController,
-            slivers: [
-              _sliverAppBarView(movie: movie),
-              _sliverBodyView(movie: movie),
-            ],
+    return Scaffold(
+      backgroundColor: gray950,
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: [
+          _sliverAppBarView(
+            movie: context.select((MovieDetailBloc bloc) => bloc.state.movieVo),
           ),
-        );
-      },
+          _sliverBodyView(),
+        ],
+      ),
     );
   }
 
-  Widget _sliverBodyView({required MovieVo movie}) {
+  Widget _sliverBodyView() {
     return SliverToBoxAdapter(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            _basicInfoView(movie: movie),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _basicInfoView({required MovieVo movie}) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              movie.title,
-              style: display3.copyWith(color: white),
-            ),
-            Row(
-              children: [
-                const Icon(Icons.star, color: yellow500, size: 22),
-                const SizedBox(width: 3),
-                Text(
-                  movie.voteAverage.toStringAsFixed(1),
-                  style: subtitle2.copyWith(color: white),
-                ),
-              ],
-            ),
-          ],
-        ),
-        const SizedBox(height: 10),
-        Row(
-          children: [
-            Expanded(
-              child: Text(
-                '${movie.releaseDate.getYearFromString()} '
-                '· ${_countryText(countries: movie.originCountry)} '
-                '· ${_genreText(genres: movie.genres)}',
-                style: h1.copyWith(color: gray400),
+            _movieBasicInfoView(
+              movie: context.select(
+                (MovieDetailBloc bloc) => bloc.state.movieVo,
               ),
             ),
           ],
         ),
-        const SizedBox(height: 50),
-        Visibility(
-          visible: movie.tagline != '',
-          child: Padding(
-            padding: const EdgeInsets.only(bottom: 30),
-            child: Text(
-              '"${movie.tagline}"',
-              style: display1.copyWith(
-                color: white,
-                fontStyle: FontStyle.italic,
-              ),
-            ),
-          ),
-        ),
-        _overviewTextView(overview: movie.overview),
-        _detailInfoView(movie: movie),
-      ],
-    );
-  }
-
-  Widget _detailInfoView({required MovieVo movie}) {
-    return Row(
-      children: [
-        Expanded(
-          child: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: IntrinsicHeight(
-              child: Row(
-                children: [
-                  _detailInfoCell(
-                    title: '상영 시간',
-                    content: movie.runtime.toHoursAndMinutes(),
-                  ),
-                  _detailInfoCell(
-                    title: '개봉',
-                    content: movie.releaseDate,
-                  ),
-                  _detailInfoCell(
-                    title: '장르',
-                    content: _genreText(genres: movie.genres),
-                  ),
-                  _detailInfoCell(
-                    title: '제작 국가',
-                    content: _countryText(countries: movie.originCountry),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _detailInfoCell({
-    required String title,
-    required String content,
-  }) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-      color: gray900.withOpacity(0.9),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            title,
-            style: bodyS.copyWith(color: white),
-          ),
-          const SizedBox(height: 9),
-          Text(
-            content,
-            style: subtitle3.copyWith(color: white),
-          ),
-        ],
       ),
     );
-  }
-
-  Widget _overviewTextView({required String overview}) {
-    return Visibility(
-      visible: overview != '',
-      child: Padding(
-        padding: const EdgeInsets.only(bottom: 50),
-        child: ExpandableTextView(
-          text: overview,
-          textStyle: bodyM.copyWith(color: white),
-          moreTextStyle: h2.copyWith(color: gray400),
-          backColor: gray950,
-          maxLines: 3,
-        ),
-      ),
-    );
-  }
-
-  String _countryText({
-    required List<String> countries,
-  }) {
-    return countries.join('/');
-  }
-
-  String _genreText({
-    required List<GenreVo> genres,
-  }) {
-    return genres.map((genreVo) => genreVo.name).join('/');
   }
 
   Widget _sliverAppBarView({
--- lib/presentation/util/int_extension.dart
+++ lib/presentation/util/int_extension.dart
@@ -1,5 +1,8 @@
 extension IntExtension on int {
   String toHoursAndMinutes() {
+    if (this < 60) {
+      return '$this분';
+    }
     final hours = this ~/ 60;
     final minutes = this % 60;
     return '$hours시간 $minutes분';
